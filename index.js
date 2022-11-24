a333c19cf27c6ba9c03d525ce21da42a98dab6db
Iddahodhiambo <iddahawuor@yahoo.com>
2022-11-24 13:07:41 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// code your solution here
+function saturdayFun(todo) {
+    if(typeof todo === 'undefined') {
+      return `This Saturday, I want to roller-skate!`
+    }
+    else{
+      return `This Saturday, I want to ${todo}!`
+    }  
+  }
+  function mondayWork(todo) {
+    if(typeof todo === 'undefined') {
+        return `This Monday, I will go to the office.`
+    }else{
+        return `This Monday, I will ${todo}.`
+        }
+    }
+    
+const wrapAdjective = function(wrapper= "*") {
+    const message = function (string = "special"){
+        return `You are ${wrapper}${string}${wrapper}!`;
+    };
+    return message;
+}
+const result = wrapAdjective("||")
+result("a hard worker");
+
+    
+  
